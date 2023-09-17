903e033baa4f51a55ebcbee3c53fc6c3274de2c8
Win Barua <win.barua2@gmail.com>
2023-09-17 18:02:10 +0100

    Implement auth through api keys

--- speech_to_text/main.py
+++ speech_to_text/main.py
@@ -1,7 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Security
+from fastapi.security import APIKeyHeader
 from transcribe import transcribe
 
 app = FastAPI()
+api_key_header = APIKeyHeader(name='X-API-Key')
+
+# TODO: testing... need to create key in database
+api_keys = [
+    "my_api_key"
+]
+
+
+def get_api_key(api_key_header: str = Security(api_key_header)) -> str:
+    if api_key_header in api_keys:
+        return api_key_header
+
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail='Invalid or missing API key'
+    )
+
+
+@app.get('/protected')
+async def protected_route(api_key: str = Security(get_api_key)):
+    return {'message': 'Access granted'}
 
 
 @app.get('/status')
