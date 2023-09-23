7a7d011c496190e4ce063eda3289af6b3698fbf4
Win Barua <win.barua2@gmail.com>
2023-09-23 15:11:53 +0100

    Cleanup

---
+++ controllers/auth.py
@@ -0,0 +1,39 @@
+from fastapi import Depends, FastAPI, HTTPException, Security, Header
+from fastapi.security import APIKeyHeader
+from typing import Annotated, List
+from database import db_connect
+
+api_key_header = APIKeyHeader(name='x-api-key')
+db = db_connect()
+
+
+def authenticate(
+    api_key_header: str = Security(api_key_header),
+    username: Annotated[str | None, Header()] = None,
+    password: Annotated[str | None, Header()] = None
+) -> List[str]:
+    cursor = db.cursor()
+
+    query = """
+        SELECT password, api_key
+        FROM scribe.users
+        WHERE consumer = '%s';
+    """ % username
+
+    cursor.execute(query)
+
+    data = cursor.fetchone()
+
+    if data is None:
+        raise HTTPException(
+            status_code=404,
+            detail='Invalid or missing username'
+        )
+
+    if data[0] == password and data[1] == api_key_header:
+        return data
+
+    raise HTTPException(
+        status_code=403,
+        detail='Invalid/missing API key or password'
+    )
--- main.py
+++ main.py
@@ -1,44 +1,19 @@
-from fastapi import Depends, FastAPI, HTTPException, Security, Header
+from fastapi import FastAPI, Security, Header
 from fastapi.security import APIKeyHeader
-from typing import Annotated, List
 from database import db_connect
-
+from controllers.auth import authenticate
+from controllers.speech_to_text.transcribe import transcribe
 
 app = FastAPI()
 api_key_header = APIKeyHeader(name='x-api-key')
 db = db_connect()
 
 
-def authenticate(
-    api_key_header: str = Security(api_key_header),
-    username: Annotated[str | None, Header()] = None,
-    password: Annotated[str | None, Header()] = None
-) -> List[str]:
-    cursor = db.cursor()
-
-    query = """
-        SELECT password, api_key
-        FROM scribe.users
-        WHERE consumer = '%s';
-    """ % username
-
-    cursor.execute(query)
-
-    data = cursor.fetchone()
-
-    if data is None:
-        raise HTTPException(
-            status_code=404,
-            detail='Invalid or missing username'
-        )
-
-    if data[0] == password and data[1] == api_key_header:
-        return data
-
-    raise HTTPException(
-        status_code=403,
-        detail='Invalid/missing API key or password'
-    )
+@app.get('/status')
+async def status():
+    return {
+        'message': 'OK, server up and running.'
+    }
 
 
 @app.get('/protected')
@@ -47,13 +22,6 @@ async def protected_route(
 ):
     test = 'test'
     return {'message': test}
-
-
-@app.get('/status')
-async def status():
-    return {
-        'message': 'OK, server up and running.'
-    }
 
 
 @app.get('/transcribe')
