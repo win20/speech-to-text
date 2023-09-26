f504342d10705e1396d38b998c5395a329791c3a
Win Barua <win.barua2@gmail.com>
2023-09-26 18:09:00 +0100

    Fix db connection

--- app/controllers/database.py
+++ app/controllers/database.py
@@ -5,10 +5,15 @@ from pymysql import converters
 
 load_dotenv()
 
+converions = converters.conversions
+converions[pymysql.FIELD_TYPE.BIT] = lambda x: False if x == b'\x00' else True
+
 
 def db_connect():
     return pymysql.connect(host=getenv('DB_HOST'),
                            port=3306,
                            user=getenv('DB_USER'),
                            password=getenv('DB_PASSWORD'),
-                           database=getenv('DB_DATABASE'))
+                           database=getenv('DB_DATABASE'),
+                           cursorclass=pymysql.cursors.DictCursor,
+                           conv=converions)
